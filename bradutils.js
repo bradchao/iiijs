863e2d6bf75bde8b36bbe328ccec5bc8313db5f5
趙令文 <brad@brad.tw>
2020-02-05 15:44:10 +0800

    02051544

--- bradutils.js
+++ bradutils.js
@@ -1,3 +1,14 @@
 function checkTWId(id){
-    return true;
+    // 1. 長度 == 10
+    // 2. 1 = A ~ Z
+    // 3. 2 = 1 or 2
+    // 4. 3-10 = 0~9
+    // 5. 檢查碼
+
+    let ret = false;
+    if (id.match(/^[A-Z][12][0-9]{8}$/)){
+        ret = true;
+    }
+
+    return ret;
 }
