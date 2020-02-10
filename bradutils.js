74da92f6ea5d4188cfcb0d28eaf4ab01966af17a
趙令文 <brad@brad.tw>
2020-02-10 16:31:59 +0800

    02101631

--- bradutils.js
+++ bradutils.js
@@ -78,5 +78,13 @@ function clone(source){
     return target;
 }
 
+Date.prototype.getTWYear = function(){
+    return this.getFullYear()-1911;
+}
 
-
+Date.prototype.getCWeek = function(){
+    let w = this.getDay();  // 0 - 6
+    let cw = ['週日','週一','週二','週三','週四'
+    ,'週五','週六'];
+    return cw[w];
+}
