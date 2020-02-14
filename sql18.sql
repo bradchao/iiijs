da7bcdead5073c33c9d1ef7535d0eb2dcb3bdad2
趙令文 <brad@brad.tw>
2020-02-14 16:27:23 +0800

    02141627

--- sql18.sql
+++ sql18.sql
@@ -1,7 +1,7 @@
 create database mytest2;
 use mytest2;
 -- 交易機制 
-create table tb1 (id int, f1 varchar(10), icon blob);
+create table tb1 (id int, f1 varchar(10));
 create table tb2 (id int, f2 varchar(10));
 show tables;
 
@@ -21,4 +21,20 @@ update tb2 set f2='C' where id=1;
 
 rollback;
 commit;
+
+delimiter $$
+select * from tb1$$
+select * from tb2$$
+select * from tb1$$
+delimiter ;
+
+-- 定義 trigger 觸發器
+create trigger mytrigger1 after insert on tb1
+for each row
+begin
+	insert into tb2 values (2, NEW.f1);
+end;
+$$
+
+
 
